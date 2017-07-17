5e3d515115ce87ac00caeab96950269489e3a6c7
Michael Flaherty <michaelwflaherty@me.com>
2017-07-17 15:45:08 -0700

    Initial Release

---
+++ IRC-Relay/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Meebey.SmartIrc4net;
+
+namespace IRCRelay
+{
+    class Program
+    {
+        public DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        public static IrcClient IRC;
+        public static Program Instance;
+
+        public static void Main(string[] args)
+        {
+            Instance = new Program();
+            IRC = new IrcClient();
+            
+            try
+            {
+                Config.Config.Load();
+            }
+            catch
+            {
+                Console.WriteLine("Unable to load config. Ensure Settings.xml is formatted correctly.");
+                Config.Config.Default();
+                Config.Config.Instance.Save();
+                return;
+            }
+            
+            Instance.MainAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+
+            client.Log += Log;
+
+            services = new ServiceCollection().BuildServiceProvider();
+
+            await InstallCommands();
+
+            await client.LoginAsync(TokenType.Bot, Config.Config.Instance.DiscordBotToken);
+            await client.StartAsync();
+
+            new Thread(() =>
+            {
+                IRC.Encoding = System.Text.Encoding.UTF8;
+                IRC.SendDelay = 200;
+
+                IRC.ActiveChannelSyncing = true;
+
+                IRC.OnError += new ErrorEventHandler(OnError);
+                IRC.OnChannelMessage += new IrcEventHandler(OnChannelMessage);
+
+
+                int port;
+                int.TryParse(Config.Config.Instance.IRCPort, out port);
+            
+                string channel = Config.Config.Instance.IRCChannel;
+
+                try
+                {
+                        IRC.Connect(Config.Config.Instance.IRCServer, port);
+                }
+                catch (ConnectionException e)
+                {
+                    System.Console.WriteLine("couldn't connect! Reason: " + e.Message);
+                    return;
+                }
+
+                try
+                {
+                    IRC.Login("discord-relay", "Discord - IRC Relay");
+                    IRC.RfcJoin(channel);
+
+                    IRC.Listen();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+            }).Start();
+
+            await Task.Delay(-1);
+        }
+
+        public static void OnError(object sender, ErrorEventArgs e)
+        {
+            System.Console.WriteLine("Error: " + e.ErrorMessage);
+            Environment.Exit(0);
+        }
+
+        public static void OnChannelMessage(object sender, IrcEventArgs e)
+        {
+            if (e.Data.Message.StartsWith("!") || e.Data.Nick.Contains("idle"))
+            {
+                return;
+            }
+
+            SendMessageAllToTarget(Config.Config.Instance.DiscordGuildName, "<" + e.Data.Nick + "> " + e.Data.Message, Config.Config.Instance.DiscordChannelName);
+        }
+
+        public static void SendMessageAllToTarget(string targetGuild, string message, string targetChannel)
+        {
+            foreach (SocketGuild guild in Program.Instance.client.Guilds) // loop through each discord guild
+            {
+                if (guild.Name.ToLower().Contains(targetGuild.ToLower())) // find target 
+                {
+                    SocketTextChannel channel = FindChannel(guild, targetChannel); // find desired channel
+
+                    if (channel != null) // target exists
+                    {
+                        Program.Instance.Log(new LogMessage(LogSeverity.Info, "SendMsg", "Sending msg to: " + channel.Name));
+                        channel.SendMessageAsync(message);
+                    }
+                }
+            }
+        }
+
+        public static SocketTextChannel FindChannel(SocketGuild guild, string text)
+        {
+            Program.Instance.Log(new LogMessage(LogSeverity.Info, "SendMsg", "Trying to find #"+text+" in: " + guild.Name));
+
+            foreach (SocketTextChannel channel in guild.TextChannels)
+            {
+                if (channel.Name.Contains(text))
+                {
+                    return channel;
+                }
+            }
+
+            return null;
+        }
+
+        private async Task InstallCommands()
+        {
+            client.MessageReceived += HandleCommand;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+
+            var context = new CommandContext(client, message);
+
+            var result = await commands.ExecuteAsync(context, argPos, services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        public Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return null;
+        }
+    }
+}
---
+++ IRC-Relay/Settings.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace IRCRelay.Config
+{
+	public class Config
+	{
+		// ---------------------------------------------------------------------------------------
+		// Insert configurable values below:
+		public string IRCServer;
+		public string IRCPort;
+        public string IRCChannel;
+
+        public string DiscordBotToken;
+        public string DiscordGuildName;
+        public string DiscordChannelName;
+		// ---------------------------------------------------------------------------------------
+
+		// Name of configuration file.
+		[XmlIgnore]
+		public const string FileName = "Settings.xml";
+
+		// Globally accessable instance of loaded configuration.
+		[XmlIgnore]
+		public static Config Instance { get; private set; }
+
+		// Empty constructor for XmlSerializer.
+		public Config()
+		{
+		}
+
+		// Used to load the default configuration if Load() fails.
+		public static void Default()
+        {
+			Config.Instance = new Config();
+
+            Config.Instance.IRCServer = "server";
+            Config.Instance.IRCPort = "port";
+            Config.Instance.IRCChannel = "#channel";
+
+            Config.Instance.DiscordBotToken = "token";
+            Config.Instance.DiscordGuildName = "server name";
+            Config.Instance.DiscordChannelName = "text channel";
+        }
+
+    // Loads the configuration from file.
+        public static void Load()
+		{
+			var serializer = new XmlSerializer(typeof(Config));
+
+			using (var fStream = new FileStream(Config.FileName, FileMode.Open))
+				Config.Instance = (Config)serializer.Deserialize(fStream);
+		}
+
+		// Saves the configuration to file.
+		public void Save()
+		{
+			var serializer = new XmlSerializer(typeof(Config));
+
+			using (var fStream = new FileStream(Config.FileName, FileMode.Create))
+				serializer.Serialize(fStream, this);
+		}
+	}
+}
