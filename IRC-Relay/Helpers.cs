a0a6b6473c595112491bcfbd5bb23835a0322f11
Michael Flaherty <michaelwflaherty@me.com>
2017-09-25 13:39:21 -0700

    Match -'s and _'s for emoji names
    Catch more emojis. Hopefully this is the last one.

--- IRC-Relay/Helpers.cs
+++ IRC-Relay/Helpers.cs
@@ -76,7 +76,7 @@ namespace IRCRelay
         {
             string returnString = input;
 
-            Regex regex = new Regex("<:[A-Za-z0-9]+:[0-9]+>");
+            Regex regex = new Regex("<:[A-Za-z0-9-_]+:[0-9]+>");
             Match match = regex.Match(input);
             if (match.Success) // contains a mention
             {
