e1227801abbb753ba392b9c6ddf6b36d3297cf70
Headline22 <michaelwflaherty@me.com>
2018-02-02 01:28:51 -0800

    Finally replace all user/channel mentions

--- IRC-Relay/Helpers.cs
+++ IRC-Relay/Helpers.cs
@@ -34,45 +34,77 @@ namespace IRCRelay
 
         public static string MentionToUsername(string input, SocketUserMessage message)
         {
-            string returnString = message.Content;
+            Regex regex = new Regex("<@!?([0-9]+)>"); // create patern
 
-            Regex regex = new Regex("<@!?[0-9]+>");
-            Match match = regex.Match(input);
-            if (match.Success) // contains a mention
+            var m = regex.Matches(input); // find all matches
+            var itRegex = m.GetEnumerator(); // lets iterate matches
+            var itUsers = message.MentionedUsers.GetEnumerator(); // iterate mentions, too
+            int difference = 0; // will explain later
+            while (itUsers.MoveNext() && itRegex.MoveNext()) // we'll loop iterators together
             {
-                string substring = input.Substring(match.Index, match.Length);
+                var match = (Match)itRegex.Current; // C# makes us cast here.. gross
+                var user = itUsers.Current;
+                int len = match.Length;
+                int start = match.Index;
+                string removal = input.Substring(start - difference, len); // seperate what we're trying to replace
 
-                SocketUser user = message.MentionedUsers.First();
-
-
-                returnString = input.Replace(substring, user.Username);
+                /**
+                * Since we're replacing `input` after every iteration, we have to
+                * store the difference in length after our edits. This is because that
+                * the Match object is going to use lengths from before the replacments
+                * occured. Thus, we add the length and then subtract after the replace
+                */
+                difference += input.Length;
+                input = ReplaceFirst(input, removal, user.Username);
+                difference -= input.Length;
             }
 
-            return returnString;
+            return input;
         }
 
-            public static string Unescape(string input)
-            {
-                return Regex.Replace(input, @"\\([^A-Za-z0-9])", "$1");
-            }
+        public static string Unescape(string input)
+        {
+            return Regex.Replace(input, @"\\([^A-Za-z0-9])", "$1");
+        }
 
         public static string ChannelMentionToName(string input, SocketUserMessage message)
         {
-            string returnString = input;
+            Regex regex = new Regex("<#([0-9]+)>"); // create patern
 
-            Regex regex = new Regex("<#[0-9]+>");
-            Match match = regex.Match(input);
-            if (match.Success) // contains a mention
+            var m = regex.Matches(input); // find all matches
+            var itRegex = m.GetEnumerator(); // lets iterate matches
+            var itChan = message.MentionedChannels.GetEnumerator(); // iterate mentions, too
+            int difference = 0; // will explain later
+            while (itChan.MoveNext() && itRegex.MoveNext()) // we'll loop iterators together
             {
-                string substring = input.Substring(match.Index, match.Length);
+                var match = (Match)itRegex.Current; // C# makes us cast here.. gross
+                var channel = itChan.Current;
+                int len = match.Length;
+                int start = match.Index;
+                string removal = input.Substring(start - difference, len); // seperate what we're trying to replace
 
-                var chan = message.MentionedChannels.First();
-
-
-                returnString = input.Replace(substring, "#" + chan.Name);
+                /**
+                * Since we're replacing `input` after every iteration, we have to
+                * store the difference in length after our edits. This is because that
+                * the Match object is going to use lengths from before the replacments
+                * occured. Thus, we add the length and then subtract after the replace
+                */
+                difference += input.Length;
+                input = ReplaceFirst(input, removal, channel.Name);
+                difference -= input.Length;
             }
 
-            return returnString;
+            return input;
+        }
+
+        public static string ReplaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
         }
 
         // Converts <:emoji:23598052306> to :emoji:
